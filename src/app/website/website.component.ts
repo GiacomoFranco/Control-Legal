39cb90fa1f75c9f8e0f1ab03346841737f35cc0f
Santy <santiago.gil@sinapsis.global>
2024-04-02 20:34:32 -0500

    ✨ Adding the footer component and

--- src/app/website/website.component.ts
+++ src/app/website/website.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@app/website/components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-website',
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, FooterComponent],
   templateUrl: './website.component.html',
   styleUrl: './website.component.scss',
 })
