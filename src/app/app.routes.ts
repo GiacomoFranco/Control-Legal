bfba8e895c7506a66ace05ad75f4c0abcc564753
Santy <santiago.gil@sinapsis.global>
2024-04-02 22:54:35 -0500

    🔍 Adding a title to the sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ export const routes: Routes = [
       import('./website/modules/home/home.module').then((m) => m.HomeModule),
   },
   {
+    title: 'Servicios',
     path: 'servicios',
     loadChildren: () =>
       import('./website/modules/services/services.module').then(
@@ -15,24 +16,32 @@ export const routes: Routes = [
       ),
   },
   {
+    title: 'Sobre Nosotros',
     path: 'sobre-nosotros',
     loadChildren: () =>
-      import('./website/modules/about-us/about-us.module').then((m) => m.AboutUsModule),
+      import('./website/modules/about-us/about-us.module').then(
+        (m) => m.AboutUsModule
+      ),
   },
   {
+    title: 'Blog',
     path: 'blog',
     loadChildren: () =>
       import('./website/modules/blog/blog.module').then((m) => m.BlogModule),
   },
   {
+    title: 'FAQ',
     path: 'faq',
     loadChildren: () =>
       import('./website/modules/faq/faq.module').then((m) => m.FaqModule),
   },
   {
+    title: 'Contacto',
     path: 'contacto',
     loadChildren: () =>
-      import('./website/modules/contact/contact.module').then((m) => m.ContactModule),
+      import('./website/modules/contact/contact.module').then(
+        (m) => m.ContactModule
+      ),
   },
   {
     path: '**',
