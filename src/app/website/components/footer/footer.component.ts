8b50e5f0e8290446a439b4d21f991efd41402c65
Santy <santiago.gil@sinapsis.global>
2024-04-08 13:15:48 -0500

    ✨ Adding Google Maps to the website

--- src/app/website/components/footer/footer.component.ts
+++ src/app/website/components/footer/footer.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { GoogleMap, MapAdvancedMarker  } from '@angular/google-maps';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [GoogleMap, MapAdvancedMarker],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  companyPosition = { lat: 6.1912182261743745, lng: -75.57700406318196 };
 
+  options: google.maps.MapOptions = {
+    center: this.companyPosition,
+    zoom: 17,
+  };
+
+  markerOptions: google.maps.marker.AdvancedMarkerElementOptions = {
+    title: 'Control Legal',
+    position: this.companyPosition,
+  };
 }
