3106c425ce05dddf4da7bce01d81a91f0381711b
Santy <santiago.gil@sinapsis.global>
2024-04-08 22:28:22 -0500

    ♻️ Importing Google Maps API logic to the footer component

--- src/app/website/components/footer/footer.component.ts
+++ src/app/website/components/footer/footer.component.ts
@@ -1,17 +1,49 @@
-import { Component } from '@angular/core';
-import { GoogleMap, MapAdvancedMarker  } from '@angular/google-maps';
+import { AfterViewInit, Component } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [GoogleMap, MapAdvancedMarker],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.initMap()
+  }
+
+  initMap() {
+    const loader = new Loader({
+      apiKey: 'AIzaSyDo75I8yn8s-w3k88ZKtzSeUtbnC9Huh-M',
+      version: 'weekly',
+    });
+
+    const element = this;
+
+    (async function () {
+      const { Map } = await loader.importLibrary('maps');
+      const { AdvancedMarkerElement } = (await loader.importLibrary(
+        'marker'
+      )) as google.maps.MarkerLibrary;
+
+      let map = new Map(
+        document.getElementById('map') as HTMLElement,
+        element.options
+      );
+
+      const marker = new AdvancedMarkerElement({
+        map: map,
+        position: element.companyPosition,
+        title: 'Control Legal',
+      });
+    })();
+  }
+
   companyPosition = { lat: 6.1912182261743745, lng: -75.57700406318196 };
 
   options: google.maps.MapOptions = {
+    mapId: '44527b7e59b94241',
     center: this.companyPosition,
     zoom: 17,
   };
