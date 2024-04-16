845f30231fcadd9c291ac7209d9248fee11d3f8e
Santy <santiago.gil@sinapsis.global>
2024-04-15 21:34:43 -0500

    🩹 Importing Swiper styles in the bundle

--- src/app/website/shared/team-members-section/team-members-swiper/team-members-swiper.component.ts
+++ src/app/website/shared/team-members-section/team-members-swiper/team-members-swiper.component.ts
@@ -3,7 +3,6 @@ import { TeamMemberCardComponent } from '../team-member-card/team-member-card.co
 import { isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID, inject } from '@angular/core';
 import Swiper from 'swiper';
-import 'swiper/css';
 
 @Component({
   selector: 'app-team-members-swiper',
@@ -23,7 +22,7 @@ export class TeamMembersSwiperComponent implements AfterViewInit {
     this.swiper = new Swiper('.swiper', {
       slidesPerView: 4,
       slidesPerGroup: 4,
-      autoHeight: true,
+      // autoHeight: true,
       spaceBetween: 10,
       navigation: {
         nextEl: '.swiper-button-next',
@@ -33,8 +32,6 @@ export class TeamMembersSwiperComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    console.log(this.platformId);
-
     if (isPlatformBrowser(this.platformId)) {
       this.initSwiper();
     }
