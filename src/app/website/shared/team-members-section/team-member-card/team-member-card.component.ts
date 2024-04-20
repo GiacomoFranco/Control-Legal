5fb1db9a91f82b1bec497169eb2d69b87eeb1e2c
Santy <santiago.gil@sinapsis.global>
2024-04-20 01:01:56 -0500

    🚧 Setting the redirection of the cards

--- src/app/website/shared/team-members-section/team-member-card/team-member-card.component.ts
+++ src/app/website/shared/team-members-section/team-member-card/team-member-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { RedirectionButtonComponent } from '../../redirection-button/redirection-button.component';
 import { Member } from '@app/website/interfaces/member.interface';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-team-member-card',
   standalone: true,
-  imports: [RedirectionButtonComponent],
+  imports: [RedirectionButtonComponent, RouterLink, RouterLinkActive],
   templateUrl: './team-member-card.component.html',
   styleUrl: './team-member-card.component.scss',
 })
