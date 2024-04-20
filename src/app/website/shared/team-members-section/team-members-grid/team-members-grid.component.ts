1874d62303345e430d0cf9e06a45a5c957c5d780
Santy <santiago.gil@sinapsis.global>
2024-04-20 04:11:40 -0500

    ♻️ Refactoring the usage of the grid members component

--- src/app/website/shared/team-members-section/team-members-grid/team-members-grid.component.ts
+++ src/app/website/shared/team-members-section/team-members-grid/team-members-grid.component.ts
@@ -1,22 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { TeamMemberCardComponent } from '../team-member-card/team-member-card.component';
-import { Member } from '@app/website/interfaces/member.interface';
 
 @Component({
   selector: 'app-team-members-grid',
   standalone: true,
   imports: [TeamMemberCardComponent],
   template: `
-    @for (member of members; track $index) {
-      <app-team-member-card [member]="member" />
-    } @empty {
-      @for (card of [1,2,3,4]; track $index) {
-        <app-team-member-card />
-      }
+    @for (card of [1,2,3,4]; track $index) {
+      <app-team-member-card />
     }
   `,
   styleUrl: './team-members-grid.component.scss',
 })
-export class TeamMembersGridComponent {
-  @Input() members: Member[] | undefined = undefined;
-}
+export class TeamMembersGridComponent {}
