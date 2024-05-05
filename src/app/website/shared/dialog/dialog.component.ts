ac2fab5cf7b3e7a2ce06b258717a977788a3675e
Santy <santiago.gil@sinapsis.global>
2024-05-05 14:29:06 -0500

    🚧 Dialog component added

---
+++ src/app/website/shared/dialog/dialog.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="section-container">
+      <span class="dialog" [style.backgroundColor]="error ? 'var(--light-brown)' : 'var(--mint-green)'">
+        @if (error) {
+        <div class="message">
+          <img
+            src="/assets/icons/error-alert.svg"
+            alt="Alerta de error"
+          />
+          <p>
+            Presentamos un error al enviar la pregunta, contacta al
+            administrador e informale sobre el error <b>{{ error }}</b>
+          </p>
+        </div>
+        } @else {
+        <div class="message">
+          <img
+            src="/assets/icons/check-alert.svg"
+            alt="Correo enviado"
+          />
+          <p>
+            Tu pregunta se ha enviado con éxito, pronto nos comunicaremos
+            contigo
+          </p>
+        </div>
+        }
+        <img
+          class="close-button"
+          src="/assets/icons/close.svg"
+          alt="Cerrar alerta"
+        />
+      </span>
+    </div>
+  `,
+  styleUrl: './dialog.component.scss',
+})
+export class DialogComponent {
+  @Input() error: string | number;
+}
