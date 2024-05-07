2696f35542e7ba46207589de3d0b1d4ee7d81746
Santy <santiago.gil@sinapsis.global>
2024-05-07 00:45:39 -0500

    ⚡️ Implementing state management for team members section

--- src/app/website/shared/team-members-section/team-members-section.component.ts
+++ src/app/website/shared/team-members-section/team-members-section.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { Component} from '@angular/core';
 import { Observable } from 'rxjs';
-import { AsyncPipe, isPlatformBrowser } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { TeamMembersSkeletonComponent } from './team-members-skeleton/team-members-skeleton.component';
 import { TeamMembersSwiperComponent } from './team-members-swiper/team-members-swiper.component';
-import { HttpClient } from '@angular/common/http';
 import { Member } from '@app/website/interfaces/member.interface';
+import { TeamMembersService } from '@app/website/services/team-members.service';
 
 @Component({
   selector: 'app-team-members-section',
@@ -13,20 +13,7 @@ import { Member } from '@app/website/interfaces/member.interface';
   templateUrl: './team-members-section.component.html',
   styleUrl: './team-members-section.component.scss',
 })
-export class TeamMembersSectionComponent implements OnInit {
-  constructor(private http: HttpClient) {}
-
-  readonly API_URL: string = import.meta.env['NG_APP_API_URL'];
-
-  // members$: Observable<object[]> = this.http.get<any>(`${this.API_URL}members`);
-  members$: Observable<Member[]>;
-
-  platformId = inject(PLATFORM_ID);
-
-  ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      // this.members$ = this.http.get<any>('https://fakestoreapi.com/products?limit=4');
-      this.members$ = this.http.get<any>(`${this.API_URL}members`);
-    }
-  }
+export class TeamMembersSectionComponent {
+  constructor(private teamMembersService: TeamMembersService) {}
+  members$: Observable<Member[]> = this.teamMembersService.getMembers();
 }
