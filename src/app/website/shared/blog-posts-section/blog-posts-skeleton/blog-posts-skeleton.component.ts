0bc7f8603ef058213814eaa93a0ba40470a7e026
Santy <santiago.gil@sinapsis.global>
2024-05-08 18:40:36 -0500

    🚧 Creating blog post skeleton component

---
+++ src/app/website/shared/blog-posts-section/blog-posts-skeleton/blog-posts-skeleton.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-posts-skeleton',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      blog-posts-skeleton works!
+    </p>
+  `,
+  styleUrl: './blog-posts-skeleton.component.scss'
+})
+export class BlogPostsSkeletonComponent {
+
+}
