ecf0c3985486bfd3586ba61ac8a8e267b6bb2885
Santy <santiago.gil@sinapsis.global>
2024-05-20 18:36:31 -0500

    🚸 Adding loading skeletons to member component

--- src/app/website/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/website/shared/breadcrumb/breadcrumb.component.ts
@@ -10,35 +10,51 @@ import { BreadcrumbService } from '@app/website/services/breadcrumb.service';
   template: `
     <section>
       <nav class="section-container">
-        @for (breadcrumb of breadcrumbs; track $index;) {
-        @if(isLastIndex(breadcrumbs, $index)){
-        <label>{{ breadcrumb.label }}</label>
+        @if (slug !== false && !slug) {
+          <div class="skeleton">
+            @for (item of skeletonArray; track $index) {
+              <div></div>
+            }
+          </div>
         } @else {
-        <a [routerLink]="breadcrumb.url">{{ breadcrumb.label }}</a>
-        <img src="assets/icons/arrow-right-breadcrumb.svg" alt="Dirección breadcrumb" />
-        } }
+          @for (breadcrumb of breadcrumbs; track $index) {
+            @if (isLastIndex(breadcrumbs, $index)) {
+              <label>{{ breadcrumb.label }}</label>
+            } @else {
+              <a [routerLink]="breadcrumb.url">{{ breadcrumb.label }}</a>
+              <img
+                src="assets/icons/arrow-right-breadcrumb.svg"
+                alt="Dirección breadcrumb"
+              />
+            }
+          }
+        }
       </nav>
     </section>
   `,
   styleUrl: './breadcrumb.component.scss',
 })
 export class BreadcrumbComponent implements OnInit {
-  @Input() slug: string;
+  @Input() slug: string | boolean;
+  skeletonArray = Array(4);
+
   breadcrumbs: Array<{ label: string; url?: string }> = [];
 
   constructor(private breadcrumbService: BreadcrumbService) {}
 
   isLastIndex(
     array: Array<{ label: string; url?: string }>,
-    index: number
+    index: number,
   ): boolean {
     return array.length === index + 1;
   }
 
   ngOnInit(): void {
+    console.log(this.slug);
+
     this.breadcrumbs = [
       ...this.breadcrumbService.breadcrumbs,
-      { label: this.slug },
+      { label: typeof this.slug === 'string' ? this.slug : null },
     ];
   }
 }
