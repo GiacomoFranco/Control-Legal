458eb78dd7cef45733ebb1191ca36a67a79ae67f
Santy <santiago.gil@sinapsis.global>
2024-05-29 23:03:01 -0500

    🚧 Implementing banner in home component

--- src/app/website/modules/home/home.component.ts
+++ src/app/website/modules/home/home.component.ts
@@ -3,11 +3,12 @@ import { TeamMembersSectionComponent } from '@app/website/shared/team-members-se
 import { MetricsComponent } from './components/metrics/metrics.component';
 import { AskAQuestionComponent } from '@app/website/shared/ask-a-question/ask-a-question.component';
 import { BlogPostsSectionComponent } from '@app/website/shared/blog-posts-section/blog-posts-section.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TeamMembersSectionComponent, MetricsComponent, AskAQuestionComponent, BlogPostsSectionComponent],
+  imports: [BannerComponent, TeamMembersSectionComponent, MetricsComponent, AskAQuestionComponent, BlogPostsSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
