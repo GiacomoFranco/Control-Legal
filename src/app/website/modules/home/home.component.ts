f6d6d2b09f0da542787b77f3843a7645fdf425ad
Santy <santiago.gil@sinapsis.global>
2024-06-03 17:29:11 -0500

    ✨ Adding legal services to the website

--- src/app/website/modules/home/home.component.ts
+++ src/app/website/modules/home/home.component.ts
@@ -4,11 +4,19 @@ import { MetricsComponent } from './components/metrics/metrics.component';
 import { AskAQuestionComponent } from '@app/website/shared/ask-a-question/ask-a-question.component';
 import { BlogPostsSectionComponent } from '@app/website/shared/blog-posts-section/blog-posts-section.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { LegalServicesSectionComponent } from '@app/website/shared/legal-services-section/legal-services-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, TeamMembersSectionComponent, MetricsComponent, AskAQuestionComponent, BlogPostsSectionComponent],
+  imports: [
+    BannerComponent,
+    LegalServicesSectionComponent,
+    TeamMembersSectionComponent,
+    MetricsComponent,
+    AskAQuestionComponent,
+    BlogPostsSectionComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/website/modules/legal-services/legal-service/legal-service.component.ts
+++ src/app/website/modules/legal-services/legal-service/legal-service.component.ts
@@ -2,13 +2,15 @@ import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LegalServiceService } from '@app/website/services/legal-service.service';
+import { AskAQuestionComponent } from '@app/website/shared/ask-a-question/ask-a-question.component';
 import { BreadcrumbComponent } from '@app/website/shared/breadcrumb/breadcrumb.component';
+import { LegalServicesSectionComponent } from '@app/website/shared/legal-services-section/legal-services-section.component';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [BreadcrumbComponent, AsyncPipe],
+  imports: [BreadcrumbComponent, AsyncPipe, AskAQuestionComponent, LegalServicesSectionComponent],
   templateUrl: './legal-service.component.html',
   styleUrl: './legal-service.component.scss',
 })
