7ea1c362d4f1a4a3f264d3bc4a5b19edd370be09
Santy <santiago.gil@sinapsis.global>
2024-06-03 18:50:58 -0500

    🦺 Legal services section validation

--- src/app/website/shared/legal-services-section/legal-services-section.component.ts
+++ src/app/website/shared/legal-services-section/legal-services-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, input } from '@angular/core';
 import { LegalServicesCardComponent } from './legal-services-card/legal-services-card.component';
 
 @Component({
@@ -9,13 +9,17 @@ import { LegalServicesCardComponent } from './legal-services-card/legal-services
     <section class="legal-services">
       <div class="section-container">
         <div class="content">
-          <div class="title-element">
-            <h2>
-              Explorando nuestros servicios,
-              <span class="highlight-text">soluciones legales de calidad</span>
-              garantizada para ti.
-            </h2>
-          </div>
+          @if (!servicesView) {
+            <div class="title-element">
+              <h2>
+                Explorando nuestros servicios,
+                <span class="highlight-text"
+                  >soluciones legales de calidad</span
+                >
+                garantizada para ti.
+              </h2>
+            </div>
+          }
           <div class="legal-services-element">
             <div class="top-row">
               @for (service of services; track $index) {
@@ -27,12 +31,22 @@ import { LegalServicesCardComponent } from './legal-services-card/legal-services
               }
             </div>
             <div class="bottom-row">
-              <app-legal-services-card
-                [imageUrl]="serviceBottom.image"
-                [serviceName]="serviceBottom.name"
-                [serviceSlug]="serviceBottom.redirection"
-              />
-              <app-legal-services-card/>
+              @if (!servicesView) {
+                <app-legal-services-card
+                  [imageUrl]="servicesBottom.image"
+                  [serviceName]="servicesBottom.name"
+                  [serviceSlug]="servicesBottom.redirection"
+                />
+                <app-legal-services-card />
+              } @else {
+                @for (service of servicesBottom; track $index) {
+                  <app-legal-services-card
+                    [imageUrl]="service.image"
+                    [serviceName]="service.name"
+                    [serviceSlug]="service.redirection"
+                  />
+                }
+              }
             </div>
           </div>
         </div>
@@ -41,27 +55,62 @@ import { LegalServicesCardComponent } from './legal-services-card/legal-services
   `,
   styleUrl: './legal-services-section.component.scss',
 })
-export class LegalServicesSectionComponent {
-  services: any[] = [
-    {
-      name: 'Derecho Civil, Comercial y de Familia',
-      image: '/assets/images/familiar.webp',
-      redirection: 'derecho-civil-comercial-y-de-familia',
-    },
-    {
-      name: 'Derecho penal y penitenciario',
-      image: '/assets/images/penal.webp',
-      redirection: 'derecho-penal-y-penitenciario',
-    },
-    {
-      name: 'Derecho laboral y pensional',
-      image: '/assets/images/laboral.webp',
-      redirection: 'derecho-laboral-y-pensional',
-    },
-  ];
-  serviceBottom: any = {
-    name: 'Derecho empresarial',
-    image: '/assets/images/empresarial.webp',
-    redirection: 'derecho-empresarial',
-  };
+export class LegalServicesSectionComponent implements OnInit {
+  ngOnInit(): void {
+    if (this.servicesView) {
+      this.services = [
+        {
+          name: 'Derecho Civil, Comercial y de Familia',
+          image: '/assets/images/familiar.webp',
+          redirection: 'derecho-civil-comercial-y-de-familia',
+        },
+        {
+          name: 'Derecho penal y penitenciario',
+          image: '/assets/images/penal.webp',
+          redirection: 'derecho-penal-y-penitenciario',
+        },
+      ];
+
+      this.servicesBottom = [
+        {
+          name: 'Derecho empresarial',
+          image: '/assets/images/empresarial.webp',
+          redirection: 'derecho-empresarial',
+        },
+        {
+          name: 'Derecho laboral y pensional',
+          image: '/assets/images/laboral.webp',
+          redirection: 'derecho-laboral-y-pensional',
+        },
+      ];
+    } else {
+      this.services = [
+        {
+          name: 'Derecho Civil, Comercial y de Familia',
+          image: '/assets/images/familiar.webp',
+          redirection: 'derecho-civil-comercial-y-de-familia',
+        },
+        {
+          name: 'Derecho penal y penitenciario',
+          image: '/assets/images/penal.webp',
+          redirection: 'derecho-penal-y-penitenciario',
+        },
+        {
+          name: 'Derecho empresarial',
+          image: '/assets/images/empresarial.webp',
+          redirection: 'derecho-empresarial',
+        },
+      ];
+
+      this.servicesBottom = {
+        name: 'Derecho laboral y pensional',
+        image: '/assets/images/laboral.webp',
+        redirection: 'derecho-laboral-y-pensional',
+      };
+    }
+  }
+
+  @Input() servicesView: boolean = false;
+  services: any[];
+  servicesBottom: any;
 }
