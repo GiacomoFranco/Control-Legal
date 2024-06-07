e67d3f7744dc66be4d1715378f34192798d1207f
Santy <santiago.gil@sinapsis.global>
2024-06-06 22:30:33 -0500

    🚧 Updating sections in about us view

--- src/app/website/modules/about-us/about-us/about-us.component.ts
+++ src/app/website/modules/about-us/about-us/about-us.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AskAQuestionComponent } from '@app/website/shared/ask-a-question/ask-a-question.component';
+import { ExpertiseSectionComponent } from '@app/website/shared/expertise-section/expertise-section.component';
+import { TeamMembersSectionComponent } from '@app/website/shared/team-members-section/team-members-section.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [TeamMembersSectionComponent, ExpertiseSectionComponent, AskAQuestionComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
