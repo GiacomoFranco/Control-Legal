43f1fb1d2dff8491d5611708c7c1fa00bdd340f1
Santy <santiago.gil@sinapsis.global>
2024-06-06 22:30:57 -0500

    🚧 Updating sections in blog view

--- src/app/website/modules/blog/blog/blog.component.ts
+++ src/app/website/modules/blog/blog/blog.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { AskAQuestionComponent } from '@app/website/shared/ask-a-question/ask-a-question.component';
+import { BlogPostsSectionComponent } from '@app/website/shared/blog-posts-section/blog-posts-section.component';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [BlogPostsSectionComponent, AskAQuestionComponent],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.scss'
+  styleUrl: './blog.component.scss',
 })
-export class BlogComponent {
-
-}
+export class BlogComponent {}
