c39710c85dcae8708c8e0a4bfdd550585516a06c
Santy <santiago.gil@sinapsis.global>
2024-06-07 23:59:27 -0500

    🚧 Implementing category tag in blog post card component

--- src/app/website/shared/blog-posts-section/blog-posts-card/blog-post-card.component.ts
+++ src/app/website/shared/blog-posts-section/blog-posts-card/blog-post-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Post } from '@app/website/interfaces/post.interface';
+import { CategoryTagComponent } from '../../category-tag/category-tag.component';
 
 @Component({
   selector: 'app-blog-post-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CategoryTagComponent],
   templateUrl: './blog-post-card.component.html',
   styleUrl: './blog-post-card.component.scss',
 })
