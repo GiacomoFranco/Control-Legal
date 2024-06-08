1f5961de08733a73dc505840c0ed2b2bf77df29e
Santy <santiago.gil@sinapsis.global>
2024-06-08 00:00:10 -0500

    ✨ Adding intern view of blog posts

--- src/app/website/modules/blog/blog-post/blog-post.component.ts
+++ src/app/website/modules/blog/blog-post/blog-post.component.ts
@@ -6,12 +6,13 @@ import { BlogPostService } from '@app/website/services/blog-post.service';
 import { AskAQuestionComponent } from '@app/website/shared/ask-a-question/ask-a-question.component';
 import { BlogPostsSectionComponent } from '@app/website/shared/blog-posts-section/blog-posts-section.component';
 import { BreadcrumbComponent } from '@app/website/shared/breadcrumb/breadcrumb.component';
+import { CategoryTagComponent } from '@app/website/shared/category-tag/category-tag.component';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-blog-post',
   standalone: true,
-  imports: [ AskAQuestionComponent ,BlogPostsSectionComponent ,BreadcrumbComponent, AsyncPipe],
+  imports: [ AskAQuestionComponent ,BlogPostsSectionComponent ,BreadcrumbComponent, AsyncPipe, CategoryTagComponent],
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.scss',
 })
