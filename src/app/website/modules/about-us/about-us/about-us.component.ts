279345c687065e3e398a56b2251b61587a020a10
Santy <santiago.gil@sinapsis.global>
2024-06-15 19:55:07 -0500

    🚧 Using the introduction section component in about us view

--- src/app/website/modules/about-us/about-us/about-us.component.ts
+++ src/app/website/modules/about-us/about-us/about-us.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { AskAQuestionComponent } from '@app/website/shared/ask-a-question/ask-a-question.component';
 import { DefaultTitleComponent } from '@app/website/shared/default-title/default-title.component';
 import { ExpertiseSectionComponent } from '@app/website/shared/expertise-section/expertise-section.component';
+import { SectionIntroductionComponent } from '@app/website/shared/section-introduction/section-introduction.component';
 import { TeamMembersSectionComponent } from '@app/website/shared/team-members-section/team-members-section.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [ DefaultTitleComponent ,TeamMembersSectionComponent, ExpertiseSectionComponent, AskAQuestionComponent],
+  imports: [ DefaultTitleComponent , SectionIntroductionComponent,TeamMembersSectionComponent, ExpertiseSectionComponent, AskAQuestionComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
