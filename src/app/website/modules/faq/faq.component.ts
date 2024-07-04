82d71692318e0fd9c1bc7f6e47dd13275cb4c95e
Santy <santiago.gil@sinapsis.global>
2024-07-03 21:32:25 -0500

    🚧 Adding ask a question component in FAQ section

--- src/app/website/modules/faq/faq.component.ts
+++ src/app/website/modules/faq/faq.component.ts
@@ -1,6 +1,7 @@
 import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { AskAQuestionComponent } from '@app/website/shared/ask-a-question/ask-a-question.component';
 import { DefaultTitleComponent } from '@app/website/shared/default-title/default-title.component';
 import { QuestionComponent } from '@app/website/shared/questions-group/question/question.component';
 import { QuestionsGroupComponent } from '@app/website/shared/questions-group/questions-group.component';
@@ -8,7 +9,7 @@ import { QuestionsGroupComponent } from '@app/website/shared/questions-group/que
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [DefaultTitleComponent, QuestionsGroupComponent, QuestionComponent, RouterLink, RouterModule],
+  imports: [DefaultTitleComponent, QuestionsGroupComponent, QuestionComponent, AskAQuestionComponent, RouterLink, RouterModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
