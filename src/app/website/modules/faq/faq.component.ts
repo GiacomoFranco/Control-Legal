2e796addd1cc4ef06061b3e5d1ebe54fb169e5ed
Santy <santiago.gil@sinapsis.global>
2024-07-03 21:22:11 -0500

    🚧 Working in faq component structure and logic

--- src/app/website/modules/faq/faq.component.ts
+++ src/app/website/modules/faq/faq.component.ts
@@ -1,13 +1,19 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 import { DefaultTitleComponent } from '@app/website/shared/default-title/default-title.component';
+import { QuestionComponent } from '@app/website/shared/questions-group/question/question.component';
+import { QuestionsGroupComponent } from '@app/website/shared/questions-group/questions-group.component';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [DefaultTitleComponent],
+  imports: [DefaultTitleComponent, QuestionsGroupComponent, QuestionComponent, RouterLink, RouterModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
 export class FaqComponent {
-
+  constructor( viewportScroller : ViewportScroller) {
+    viewportScroller.setOffset([0, 160])
+  }
 }
