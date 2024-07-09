c5690003d11c8b981ffbeb68397f3784877dea52
Santy <santiago.gil@sinapsis.global>
2024-07-08 19:38:27 -0500

    ♻️ Refactoring textarea input

--- src/app/website/shared/form/textarea-input/textarea-input.component.ts
+++ src/app/website/shared/form/textarea-input/textarea-input.component.ts
@@ -9,7 +9,7 @@ import { FormControl, ReactiveFormsModule} from '@angular/forms';
   template: `
     <div class="input-container">
       <textarea name="" id="" cols="30" rows="1" id="contact-message" [formControl]="control" required ></textarea>
-      <label for="contact-message">Escribe tu pregunta acá</label>
+      <label for="contact-message">{{ label }}</label>
       <div class="underline"></div>
     </div>
     <ng-content select="[error]"></ng-content>
@@ -18,4 +18,5 @@ import { FormControl, ReactiveFormsModule} from '@angular/forms';
 })
 export class TextareaInputComponent {
     @Input() control: FormControl = new FormControl();
+    @Input() label: string;
 }
