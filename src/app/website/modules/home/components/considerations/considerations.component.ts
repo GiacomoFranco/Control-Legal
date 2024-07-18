096cef29cc47c7068f6bc881c1999865c8adc7de
Santy <santiago.gil@sinapsis.global>
2024-07-17 21:41:20 -0500

    ✨ Adding considerations component

---
+++ src/app/website/modules/home/components/considerations/considerations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-considerations',
+  standalone: true,
+  imports: [],
+  templateUrl: './considerations.component.html',
+  styleUrl: './considerations.component.scss'
+})
+export class ConsiderationsComponent {
+
+}
